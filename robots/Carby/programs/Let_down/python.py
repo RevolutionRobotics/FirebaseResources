f8346da1eb4c11ca5507ad6652e9dea3eb3903a1
david.dudas <david.dudas@outlook.com>
2019-12-13 17:13:15 +0100

    Carby default programs

---
+++ robots/Carby/programs/Let_down/python.py
@@ -0,0 +1,1 @@
+robot.motors["lift"].move(direction=Motor.DIRECTION_FWD, amount=370, unit_amount=Motor.UNIT_DEG, limit=20, unit_limit=Motor.UNIT_SPEED_PWR)
---
+++ robots/Carby/programs/Open/python.py
@@ -0,0 +1,1 @@
+robot.motors["gripper"].move(direction=Motor.DIRECTION_BACK, amount=90, unit_amount=Motor.UNIT_DEG, limit=20, unit_limit=Motor.UNIT_SPEED_RPM)

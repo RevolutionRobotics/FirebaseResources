fc0184335705a723d086dac8af7a2c5d1efcde82
david.dudas <david.dudas@outlook.com>
2020-01-24 15:24:22 +0100

    Launcher background program fix, carby minor fix

--- robots/Carby/programs/Lift/python.py
+++ robots/Carby/programs/Lift/python.py
@@ -1,1 +1,2 @@
 robot.motors["lift"].move(direction=Motor.DIRECTION_BACK, amount=370, unit_amount=Motor.UNIT_DEG, limit=20, unit_limit=Motor.UNIT_SPEED_RPM)
+time.sleep(1)
--- robots/Launcher/programs/spin-shooters-with-button/python.py
+++ robots/Launcher/programs/spin-shooters-with-button/python.py
@@ -9,9 +9,9 @@ while True:
   if (robot.sensors["bumper"].read()) and ((robot.time()) - last_pressed > 1):
     last_pressed = robot.time()
     spinning = not spinning
-  if spinning:
-    robot.motors["left"].spin(direction=Motor.DIRECTION_FWD, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
-    robot.motors["right"].spin(direction=Motor.DIRECTION_BACK, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
-  else:
-    robot.stop_all_motors(action=Motor.ACTION_STOP_AND_HOLD)
+    if spinning:
+      robot.motors["left"].spin(direction=Motor.DIRECTION_FWD, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
+      robot.motors["right"].spin(direction=Motor.DIRECTION_BACK, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
+    else:
+      robot.stop_all_motors(action=Motor.ACTION_STOP_AND_HOLD)
   time.sleep(0.05)  # allow other threads to run

b6a11594202ba8873914f8ada88d4f9713618fa2
Balázs Maczák <maczak.balazs@gmail.com>
2020-02-06 16:12:45 +0100

    Fixed Launcher default programs

--- robots/Launcher/programs/spin-shooters-with-button/python.py
+++ robots/Launcher/programs/spin-shooters-with-button/python.py
@@ -1,18 +1,14 @@
-import time
-
-spinning = None
-lastPressed = None
-
-
-spinning = False
-lastPressed = robot.time()
 while True:
-  if (robot.sensors["button"].read()) and ((robot.time()) - lastPressed > 1):
-    lastPressed = robot.time()
-    spinning = not spinning
-    if spinning:
-      robot.motors["left"].spin(direction=Motor.DIRECTION_FWD, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
-      robot.motors["right"].spin(direction=Motor.DIRECTION_BACK, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
-    else:
-      robot.stop_all_motors(action=Motor.ACTION_RELEASE)
+  while robot.sensors["button"].read():
+    time.sleep(0.05)  # allow other threads to run
+  while not robot.sensors["button"].read():
+    time.sleep(0.05)  # allow other threads to run
+  robot.motors["left"].spin(direction=Motor.DIRECTION_FWD, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
+  robot.motors["right"].spin(direction=Motor.DIRECTION_BACK, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
+  while robot.sensors["button"].read():
+    time.sleep(0.05)  # allow other threads to run
+  while not robot.sensors["button"].read():
+    time.sleep(0.05)  # allow other threads to run
+  for motor in robot.motors:
+    motor.stop(action=Motor.ACTION_RELEASE)
   time.sleep(0.05)  # allow other threads to run
--- robots/Launcher/programs/start/python.py
+++ robots/Launcher/programs/start/python.py
@@ -1,3 +1,6 @@
+import time
+
+
 robot.motors["left"].spin(direction=Motor.DIRECTION_FWD, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
 robot.motors["right"].spin(direction=Motor.DIRECTION_BACK, rotation=150, unit_rotation=Motor.UNIT_SPEED_RPM)
 time.sleep(2)
